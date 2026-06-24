98f578827ae846e9332dab742a50a50a8fdfc9c8
banne227 <baidyanne9@gmail.com>
2026-06-24 15:42:07 +0200

    add login, register method

--- services/game_server/src/api.ts
+++ services/game_server/src/api.ts
@@ -1,5 +1,53 @@
 import { state} from './game'
 
+//j'envoie une requete POST pour connecter un player a la db
+export async function login(email : string, password : string) {
+    const response = await fetch("http://api:4444/login",
+        {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+        }),
+        }
+    );
+
+    const data = await response.json(); // récupère la réponse du serveur
+    if (!response.ok) {
+        console.error("Login échoué:", data.message);
+        return null;
+    }
+    localStorage.setItem("username", data.username); // stocke le nom
+    return data; // retourne les infos du joueur
+}
+
+//j'envoie une requete POST pour ajouter un player a la db
+export async function register(id: string, email : string, password : string) {
+    const response = await fetch("http://api:4444/register",
+        {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            username: state.players[id]?.name,
+            email: email,
+            password: password,
+        }),
+        }
+    );
+
+    const data = await response.json(); // récupère la réponse du serveur
+
+    if (!response.ok) {
+        console.error("register échoué:", data.message);
+        return null;
+    }
+    return data; // retourne les infos du joueur
+}
 
 //j'envoie une requete POST pour ajouter le score de mon player a la db
 export function addScore(id: string, to_add : number) {
